b3f951e7cf8b2c25283eaa19bb2853fe69c851ce
Reza Fatahi <aug2uag@gmail.com>
2017-10-14 16:15:01 -0700

    update profile

--- client/views/main/templates/profile/settings.js
+++ client/views/main/templates/profile/settings.js
@@ -1,3 +1,13 @@
+function validateUrl(value) {
+	return true // how to handle username ?
+	return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
+function deleteRow(e) {
+	e.preventDefault();
+	$(this).closest('tr').remove();
+}
+
 var osettings = {};
 osettings.avatar = {};
 
@@ -17,136 +27,72 @@ function guid() {
 }
 
 Template.settings.events({
-	'click #update': function(e) {
+	'click #save_settings': function(e) {
 		e.preventDefault();
+
+
+		/**
+			firstname
+			lastname
+			bio
+			category -- primaryRole
+			user-role -- checkboxes //array
+			needs-val  == table //array
+			social-val  == table //array
+			reel-val  == table //array
+
+		*/
+
+
 		var o = {};
 		o.firstName = $('#first_name').val();
 		o.lastName = $('#last_name').val();
-		o.iam = [];
-		$('.iam:checked').each(function() {
-		    o.iam.push($(this).attr('value'));
-		});
-		o.interests = [];
-		$('.interests:checked').each(function() {
-		    o.interests.push($(this).attr('value'));
-		});
-		var url = $('#website').val();
-		if (url && !isURL(url)) {
-			bootbox.alert('Invalid URL, please include your website in "http://" or "https://" format for the URL.');
-			return;
-		};
-		o.website = url;
-		o.avatar = osettings.avatar;
 		o.bio = $('#bio').val();
+		o.primaryRole = $('#category').find(":selected").text();
+		o.iam = [];
+		o.assets = [];
+		o.social = [];
+		o.reels = [];
+		o.avatar = osettings.avatar;
+
+		var userRoles = $('.user-role');
+		userRoles.each(function(idx, el) {
+			if ($(el).prop('checked')) o.iam.push($(el).attr('name'));
+		});
+
+		var userAssets = $('.needs-val');
+		userAssets.each(function(i, el) {
+			var _o = {};
+			var arr = $(el).children('td');
+			_o.category = $(arr[0]).text();
+			_o.description = $(arr[1]).text();
+			_o.quantity = $(arr[2]).text();
+			o.assets.push(_o);
+		});
+
+		var userSocial = $('.social-val');
+		userSocial.each(function(i, el) {
+			var _o = {};
+			var arr = $(el).children('td');
+			_o.name = $(arr[0]).text();
+			_o.address = $(arr[1]).text();
+			o.social.push(_o);
+		});
+
+		var userReels = $('.reel-val');
+		userReels.each(function(i, el) {
+			var _o = {};
+			var arr = $(el).children('td');
+			_o.name = $(arr[0]).text();
+			_o.address = $(arr[1]).text();
+			o.reels.push(_o);
+		});
+
+		console.log(new Array(100).join('*'))
+		console.log(o)
+
 		Meteor.call('upgradeProfile', o);
 
-	},
-	'click #works_url_submit': function(e) {
-		e.preventDefault();
-		// collect URL/text, description
-		// validate format of URL
-		// add to global object
-		// get #works_url and add to list
-
-		// show dialog for uploading text/URL
-		// uploading description of work
-		// uploading date of work
-
-		bootbox.dialog({
-			message: '<label>Include the URL, description, and date of your work<br></label><input type="text" id="works_url" name="works_url" placeholder="Enter URL"><input type="text" id="works_description" name="works_description" placeholder="Description of your work"><input type="date" id="works_date" name="works_date" placeholder="Date of your work">',
-			buttons: {
-		        confirm: {
-		            label: 'Add',
-		            className: 'btn-success',
-		            callback: function(e) {
-		            	e.preventDefault();
-		            	// scrape the URL, description, and date
-		            	// add to data source
-		            	// add to table
-		            	var o = {
-		            		url: $('#works_url').val(),
-		            		description: $('#works_description').val() || 'no description provided',
-		            		date: $('#works_date').val() || new Date(),
-		            		id: guid() + new Date().getTime()
-		            	}
-
-		            	if (!o.url) return bootbox.alert('you must specify a URL for this to work');
-
-		            	Meteor.call('updateList', 'onlineWorks', 'Your online works list', o);
-		            }
-		        },
-		        cancel: {
-		            label: 'Cancel',
-		            className: 'btn-danger'
-		        }
-		    }
-		})
-	},
-	'click .deleteWorksRow': function(e) {
-		Meteor.call('removeFromList', 'onlineWorks', 'Your online works list', 'id', $(this)[0].id);
-	},
-	'click .deleteResourceRow': function(e) {
-		Meteor.call('removeFromList', 'resources', 'Your resource list', 'id', $(this)[0].id);
-	},
-	'click .deleteSocialRow': function(e) {
-		Meteor.call('removeFromList', 'socialLinks', 'Your social media links list', 'id', $(this)[0].id);
-	},
-	'click .deleteHeadshotsRow': function(e) {
-		Meteor.call('removeFromList', 'headshots', 'Your personal files list', 'id', $(this)[0].url);
-	},
-	'click #social_media_submit': function(e) {
-		e.preventDefault();
-		var url = $('#social_media').val();
-		if (!isURL(url)) {
-			bootbox.alert('Invalid URL, please copy & paste the link that includes "http://" or "https://" for the URL.');
-			return;
-		};
-		$('#social_media').val('');
-		Meteor.call('updateList', 'socialLinks', 'Your social media links list', {
-			url: url,
-			id: guid()
-		});
-
-	},
-	'click #headshots_submit': function(e) {
-		e.preventDefault();
-		if (Object.keys(osettings).length === 0) return;
-		var reader = new FileReader();
-        osettings.date = $('#headshots_date').val() && new Date($('#headshots_date').val()) || new Date(file.lastModifiedDate);
-	    osettings.name = $('#headshots_name').val() || file.name;
-	    osettings.description = $('#headshots_description').val() || 'no description was provided for this item';
-	    osettings.tags = [];
-	    $('.select_tag.active').each(function(idx,el) {
-	    	osettings.tags.push($(el).html());
-	    });
-		reader.onload = function(readerEvt) {
-            osettings.data = readerEvt.target.result;
-	        delete osettings['file'];
-	        var o = new Object(osettings);
-	        delete o['avatar'];
-			Meteor.call('uploadSettingFiles', 'headshots', osettings);
-			$('#headshots_date').val('');
-		    $('#headshots_name').val('');
-		    $('#headshots_description').val('');
-		    $('#fileInput').val('');
-		    $('.select_tag.active').each(function(idx, el) {
-		    	$(el).toggleClass("active");
-		    });
-        }; 
-    	
-        reader.readAsDataURL(osettings.file);
-	},
-	'click .select_tag': function(e) {
-		e.preventDefault();
-		$(e.target).toggleClass("active");
-	},
-	'click #resource_submit': function(e) {
-		e.preventDefault();
-		Meteor.call('updateList', 'resources', 'Your resources list', {
-			resource: $( "#object_category option:selected" ).text(),
-			details: $("#resource_details").val(),
-			id: guid()
-		});
 	},
 	'change #avatar_file': function (e, template) {
 	    if (e.currentTarget.files && e.currentTarget.files[0]) {
@@ -166,65 +112,43 @@ Template.settings.events({
     		reader.readAsDataURL(file);
 	  	}
 	},
-	'click .remove_headshot': function(e) {
-		Meteor.call('removeFromList', 'headshots', 'Your personal files list', 'url', this.url.split('https://s3-us-west-2.amazonaws.com/producehour/headshots/')[1]);
-	}
+	'click #add-needs': function(e) { 
+      e.preventDefault();
+      var cat = $('#needs-category').val(), description = $('#needs-description').val(), quantity = parseInt($('#needs-quantity').val());
+      if (cat.toLowerCase().indexOf('category')>-1) return;
+      if (typeof quantity !== 'number' || quantity<1) return;
+      if (cat && description && quantity) $('#needs-table').append('<tr class="needs-val"><td>'+cat+'</td><td>'+description+'</td><td>'+quantity+'</td><td><button class="deleteRow button small">X</button></td></tr>');
+      $('.deleteRow').on('click', deleteRow);
+      $("#needs-category").val($("#needs-category option:first").val()), $('#needs-description').val(''), $('#needs-quantity').val('');
+    },
+    'click #add-social': function(e) { 
+      e.preventDefault();
+      var title = $('#social-title').val(), url = $('#social-url').val();
+      if (title && url && validateUrl(url)) $('#social-table').append('<tr class="social-val"><td>'+title+'</td><td>'+url+'</td><td><button class="deleteRow button small">X</button></td></tr>');
+      $('.deleteRow').on('click', deleteRow);
+      $('#social-title').val(''), $('#social-url').val('');
+    },
+    'click #add-reel': function(e) { 
+      e.preventDefault();
+      var title = $('#reel-title').val(), url = $('#reel-url').val();
+      if (title && url && validateUrl(url)) $('#reel-table').append('<tr class="reel-val"><td>'+title+'</td><td>'+url+'</td><td><button class="deleteRow button small">X</button></td></tr>');
+      $('.deleteRow').on('click', deleteRow);
+      $('#reel-title').val(''), $('#reel-url').val('');
+    }
 });
 
 Template.settings.helpers({
+	bio: function() {
+		return Meteor.user().bio || 'describe yourself and your experiences'
+	},
 	first_name: function() {
-		return Meteor.user().firstName || 'enter first name';
+		return Meteor.user().firstName || 'First name';
 	},
 	last_name: function() {
-		return Meteor.user().lastName || 'enter last name';
+		return Meteor.user().lastName || 'Last name';
 	},
 	avatar: function() {
 		return Meteor.user().avatar;
-	},
-	worksExamples: function() {
-		return Meteor.user().onlineWorks && Meteor.user().onlineWorks.length > 0;
-	},
-	worksList: function() {
-		return Meteor.user().onlineWorks;
-	},
-	headfiles: function() {
-		return Meteor.user().headshots && Meteor.user().headshots.length > 0;	
-	},
-	headshots: function() {
-		return Meteor.user().headshots.map(function(i) {
-			i.url = 'https://s3-us-west-2.amazonaws.com/producehour/headshots/' + i.url;
-			return i;
-		});
-	},
-	resources: function() {
-		return Meteor.user().resources && Meteor.user().resources.length > 0;
-	},
-	resourceList: function() {
-		return Meteor.user().resources;
-	},
-	socialLinks: function() {
-		return Meteor.user().socialLinks && Meteor.user().socialLinks.length > 0;
-	},
-	socialLinksList: function() {
-		return Meteor.user().socialLinks;
-	},
-	isViewer: function() {
-		return Meteor.user().iam.indexOf('viewer') > -1;
-	},
-	isWriter: function() {
-		return Meteor.user().iam.indexOf('writer') > -1;
-	},
-	isDirector: function() {
-		return Meteor.user().iam.indexOf('director') > -1;
-	},
-	isProducer: function() {
-		return Meteor.user().iam.indexOf('producer') > -1;
-	},
-	isActor: function() {
-		return Meteor.user().iam.indexOf('actor') > -1;
-	},
-	isCinematographer: function() {
-		return Meteor.user().iam.indexOf('cinematographer') > -1;
 	}
 });
 
@@ -246,8 +170,8 @@ Template.settings.rendered = function () {
   var wbs = Meteor.user().website && Meteor.user().website.length > 0 ? Meteor.user().website : 'http://yoursite';
   $('#website').attr('placeholder', wbs);
 
-  var _bio = Meteor.user().bio && Meteor.user().bio.length > 0 ? Meteor.user().bio : 'describe yourself and experiences';
-  $('#bio').attr('placeholder', _bio);
+  // var _bio = Meteor.user().bio && Meteor.user().bio.length > 0 ? Meteor.user().bio : 'describe yourself and experiences';
+  // $('#bio').attr('placeholder', _bio);
 };
 
 
