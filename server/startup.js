12830c589930551273b861742dd24f49fa0536ad
Rex Fatahi <rex@lionrt.com>
2018-05-08 22:25:30 -0700

    alula jpeg and cors header update to prod

--- server/startup.js
+++ server/startup.js
@@ -2,8 +2,7 @@ var connectHandler = WebApp.connectHandlers;
 
 Meteor.startup(function () {
   connectHandler.use(function (req, res, next) {
-    res.setHeader('access-control-allow-origin', 'http://localhost:3000');
-    console.log(new Array(100).join('@!'))
+    res.setHeader('access-control-allow-origin', 'https://app.opensourcehollywood.org/');
     return next();
   })
 })
