29ba6d80c4f5ee25329529dc3002d19a1caa8104
Rex Fatahi <rex@lionrt.com>
2018-05-19 02:21:07 -0700

    disqus config fix

--- client/views/main/events.js
+++ client/views/main/events.js
@@ -269,6 +269,18 @@ Template.blog.onRendered(function() {
       $('.pinterest-share').html('<li class="fa fa-pinterest"></li>');
       $('.googleplus-share').html('<li class="fa fa-google-plus"></li>');
    }, 133);
+
+
+    var disqus_config = function () {
+      this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = this.data._id; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+      s.src = 'https://open-source-hollywood-1.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
  });
 
 Template.bloglist.helpers({
