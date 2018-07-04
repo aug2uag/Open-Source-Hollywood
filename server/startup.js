acc8c7e1967617ee172eb4539036b6633e0f8a47
Rex Fatahi <rex@lionrt.com>
2018-07-03 21:08:58 -0700

    trying to fix serviceconfiguration bug

--- server/startup.js
+++ server/startup.js
@@ -4,5 +4,13 @@ Meteor.startup(function () {
   connectHandler.use(function (req, res, next) {
     res.setHeader('access-control-allow-origin', '*');
     return next();
-  })
+  });
+
+  ServiceConfiguration.configurations.remove({ service: 'auth0' });
+	ServiceConfiguration.configurations.insert({
+	  service:      'auth0',
+	  domain:       'aug2uag.auth0.com',
+	  clientId:     'KC97k4Aq9IbWuNuC8YKzC4IxSPaJWtzB',
+	  clientSecret: 'ZwboYVYBP49egRZLTBXf6FwBcE4OGnguihGYv3zzmSbdbpnjFXiE8vyWPPJQ7ieb'
+	});
 })
