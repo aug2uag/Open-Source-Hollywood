3dd9c615d0ca5349baef6f2f25385cc91fa4281f
Rex Fatahi <rex@lionrt.com>
2019-02-14 20:03:23 -0800

    contract view convenience

--- client/views/projects/templates/applicants.js
+++ client/views/projects/templates/applicants.js
@@ -1,3 +1,9 @@
+function iAmOfferee(offereeId) {
+	if (Meteor.user()._id===offereeId) return true;
+	return false
+}
+
+
 Template.applicantsContracts.helpers({
 	foo: function() {
 		console.log(this)
@@ -26,5 +32,41 @@ Template.applicantsContracts.helpers({
 	},
 	roleSummary: function() {
 		return [this.message, 'for role:', this.appliedFor].join(' ')
-	}
+	},
+	thisString: function() {
+		return JSON.stringify(this)
+	},
+	contractLink: function() {
+		if (this.ctx&&this.ctx==='offer') {
+			if (!this.slug) {
+				if (iAmOfferee(this.offeree)) {
+					return ['/message/project/', this.offeror, '/', this._id].join('')
+				} else {
+					return ['/message/project/', this.offeree, '/', this._id].join('')
+				}
+			};
+			return ['/message/project/', this.slug, '/', this._id].join('')
+		};
+		// console.log(this)
+		var userId = typeof this.user === 'string' ? this.user : this.offer&&this.offer.user ? this.offer.user.id : ''
+		if (!userId) return false;
+		return ['/message/project/', this.slug, '/', userId].join('')
+	},
+	offerName: function() {
+		if (iAmOfferee(this.offeree)) {
+			return this.offer.user.name	
+		};
+
+		if (this.title) return this.title
+
+		if (this.offer) {
+			if (this.offer.user&&this.offer.user.name) {
+				return [this.offer.user.name, this.offer.message].join(' ')
+			};
+
+			if (this.offer.message) return this.offer.message;
+		};
+		
+		return 'Active Contract Negotiation'
+	},
 })
